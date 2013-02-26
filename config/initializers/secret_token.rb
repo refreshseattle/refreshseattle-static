8c69871e2390ee89c8d5cb3b8c69dc73ba5c04f3
Mike Tierney <mike@panpainter.com>
2013-02-26 10:02:28 -0700

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RefreshSeattle::Application.config.secret_token = 'f41e5041fde4445378222d04a46ed08fd8bd58e91b9b0c2a915c32ccba07e459ea402ffef810fad0f23215e8f5d557f7f5d287dd847aa9c421f6f90f0748dcbf'
