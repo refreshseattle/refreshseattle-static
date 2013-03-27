0ec079c573b122fd210114aa7afca4f50cb25b22
Mike Tierney <mike@panpainter.com>
2013-03-26 19:56:35 -0700

    Run the Foundation generator.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,4 +10,9 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require jquery
+//= require foundation
 //= require_tree .
+
+// FIXME: Get the Foundation js working
+// $(document).foundation();
